c6b0ac74971ed2c432923afc131db64898319899
plumstyle <anxxx154@morris.umn.edu>
2018-09-25 15:14:51 -0500

    1. fixed main.c by correcting the free function; 2. implemented check-vowel function for disemvowel.c; 3. corrected disemvowel to fit gtest and valgrind test.

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -2,6 +2,41 @@
 
 #include "disemvowel.h"
 
-char *disemvowel(char *str) {
-  return (char*) "";
+// takes a char check if it is a vowel
+int check_vowel(char c) {
+  switch(c){
+  case 'A':
+  case 'a':
+  case 'E':
+  case 'e':
+  case 'I':
+  case 'i':
+  case 'O':
+  case 'o':
+  case 'U':
+  case 'u':
+    return 1;
+  default:
+    return 0;
+  }
 }
+
+// takes a sequence of chars and return a sequence of chars that does not contain vowels
+char *disemvowel(char *str) {
+  char *modline;
+  int i, j = 0;
+  size_t size;
+
+  // creating an target char sequence as output
+  size = 100;
+  modline = (char*) malloc(sizeof(size + 1));
+  for (i = 0; str[i] !='\0'; i++) {
+    if (check_vowel(str[i]) == 0) {
+      modline[j] = str[i];
+      j++;
+    }
+  }
+  modline[j] = '\0';
+  return modline;
+}
+
